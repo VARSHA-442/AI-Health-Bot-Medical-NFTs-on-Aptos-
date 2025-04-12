650f7afdf62154b537b8256a2da6f5889c1e7049
Venu-16 <venumadhav2212@gmail.com>
2025-04-12 18:33:11 +0530

    model

--- model/model.py
+++ model/model.py
@@ -6,7 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pickle
 
-df = pd.read_csv('/workspaces/AI-Health-Bot-Medical-NFTs-on-Aptos-/dataset/dataset10.csv')
+df = pd.read_csv('C:\\Users\\venum\\PycharmProjects\\hack\\AI-Health-Bot-Medical-NFTs-on-Aptos-\\dataset\\dataset10.csv')
 train=df.drop('diseases', axis=1)
 
 x_train, x_test, y_train, y_test = train_test_split(train, df['diseases'], test_size=0.2, random_state=42)
